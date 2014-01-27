87c8fde211a59c152a7d3e7b7a33c09c07d9e0ac
Jon Edvardsson <jon.edvardsson@tdc.se>
2014-01-27 10:51:37 +0100

    Adding support for file format selection.

--- webseqdiagram.sh
+++ webseqdiagram.sh
@@ -6,36 +6,40 @@ showhelp() {
 cat <<EOF
 Generates a PNG file from the given input_file using WebSequenceDiagram.
 
-Usage: $PROG style input_file
+Usage: $PROG [options] input_file
 
-    -d          target dir
+    -d|--dir            target dir
+    -s|--style          diagram style, default "rose".
+    -f|--format         output file format, default "png".
 
-	style		the web sequence diagram style parameter (e.g. rose)
-	input_file	the sequence diagram input file
+	input_file	        the sequence diagram input file
 EOF
     exit;
 }
 
 dir='.'
+style='rose'
+format='png'
 for i
 do
     case $1 in
         -h|--help) showhelp; exit 0;;
-        -d) dir=$2; shift 2;;
+        -d|--dir) dir=$2; shift 2;;
+        -f|--format) format=$2; shift 2;;
+        -s|--style) style=$2; shift 2;;
         -*) echo "Unknown option: $1"; exit 1;;
         *) break;;
     esac
 done
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 1 ]; then
     showhelp;
     exit 1;
 fi
 
 GENERATOR_URL="https://www.websequencediagrams.com/index.php"
-style=$1
-source_file=$2
-png_file=$dir/${source_file%.*}.png
+source_file=$1
+dest_file=$dir/${source_file%.*}.$format
 
 result=$(curl -sS -k "$GENERATOR_URL" \
     --data-urlencode "style=$style" \
@@ -43,22 +47,22 @@ result=$(curl -sS -k "$GENERATOR_URL" \
     --data-urlencode "paginate=0" \
     --data-urlencode "paper=letter" \
     --data-urlencode "landscape=0" \
-    --data-urlencode "format=png" \
+    --data-urlencode "format=$format" \
     --data-urlencode "apiVersion=1" \
-    --data-urlencode "width=830" \
     --data-urlencode "message@$source_file")
 
+echo $result
 if echo $result |grep -Pqv '"errors": \[\]'; then
     echo "There were errors:"
     echo $result
     exit 1
 fi
 
-web_file=$(echo $result | grep -o '\?png=[a-zA-Z0-9]*')
+web_file=$(echo $result | grep -o "\?$format=[a-zA-Z0-9]*")
 
 if [ -z $web_file ]; then
-	echo "Could not generate diagram: $result"
+	echo "Could find resulting file: $result"
 	exit 1
 fi
 
-wget --no-check-certificate "$GENERATOR_URL$web_file" -O $png_file
+wget --no-check-certificate "$GENERATOR_URL$web_file" -O $dest_file
